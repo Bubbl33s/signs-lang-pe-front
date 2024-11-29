62f1e76df58ca3ea7e7be993fa02161eda9c0cac
Bubbl33s <jairo.lf.8@gmail.com>
2024-11-29 18:03:00 -0500

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from './components';
+
 function App() {
-  return <>Hello World!</>;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-100 to-green-100 p-3">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
