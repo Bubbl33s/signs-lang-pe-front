878211277b93f1c9881caae0d42b85c76414ee61
Bubbl33s <jairo.lf.8@gmail.com>
2024-12-10 21:00:15 -0500

    feat: add footer responsivity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,47 +26,44 @@ export default function Footer() {
   };
 
   return (
-    <>
-      <footer className="bg-purple-600 text-white p-3">
-        <div className="flex gap-2 justify-center">
-          <HandIndex className="text-xl" />
-          <h2 className="text-lg font-semibold">Lengua de Señas Peruana</h2>
-        </div>
-        <section className="flex justify-center gap-4 mt-2">
-          <button
-            onClick={() => openModal('Sobre este proyecto', <AboutUs />)}
-            className="font-medium hover:text-purple-200 hover:scale-105 transition-all"
-          >
-            Sobre este proyecto
-          </button>
+    <footer className="bg-purple-600 text-white p-6">
+      <div className="flex gap-2 justify-center mb-4">
+        <HandIndex className="text-xl" />
+        <h2 className="text-lg font-semibold">Lengua de Señas Peruana</h2>
+      </div>
+      <section className="mt-2 overflow-hidden grid grid-cols-2 gap-3 md:flex md:justify-center md:gap-6">
+        <button
+          onClick={() => openModal('Sobre este proyecto', <AboutUs />)}
+          className="font-medium hover:text-purple-200 hover:scale-105 transition-all underline"
+        >
+          Sobre este proyecto
+        </button>
 
-          <button
-            onClick={() => openModal('Cómo contribuir', <HowToContribute />)}
-            className="font-medium hover:text-purple-200 hover:scale-105 transition-all"
-          >
-            Cómo contribuir
-          </button>
+        <button
+          onClick={() => openModal('Cómo contribuir', <HowToContribute />)}
+          className="font-medium hover:text-purple-200 hover:scale-105 transition-all underline"
+        >
+          Cómo contribuir
+        </button>
 
-          <button
-            onClick={() =>
-              openModal('La importancia de contribuir', <Importance />)
-            }
-            className="font-medium hover:text-purple-200 hover:scale-105 transition-all"
-          >
-            La importancia de contribuir
-          </button>
+        <button
+          onClick={() =>
+            openModal('La importancia de contribuir', <Importance />)
+          }
+          className="font-medium hover:text-purple-200 hover:scale-105 transition-all underline"
+        >
+          La importancia de contribuir
+        </button>
 
-          <button
-            onClick={() =>
-              openModal('La importancia de contribuir', <Scalabilty />)
-            }
-            className="font-medium hover:text-purple-200 hover:scale-105 transition-all"
-          >
-            Escalabilidad
-          </button>
-        </section>
-      </footer>
-
+        <button
+          onClick={() =>
+            openModal('La importancia de contribuir', <Scalabilty />)
+          }
+          className="font-medium hover:text-purple-200 hover:scale-105 transition-all underline"
+        >
+          Escalabilidad
+        </button>
+      </section>
       <InfoModal
         isOpen={isOpen}
         onClose={closeModal}
@@ -74,6 +71,6 @@ export default function Footer() {
       >
         {modalContent.content}
       </InfoModal>
-    </>
+    </footer>
   );
 }
